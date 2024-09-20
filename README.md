37836b4f43b18036170972985d594491fac1f25a
Mário <brifanz@hotmail.com>
2024-09-20 20:03:46 +0100

    backend implementation

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # full-stack-exercise-1223
-Altair.io full-stack-exercise-1223
+Altair.io full-stack exercise
+
+## Structure
+
+/backend
+
+### Backend
+
+To install/run the backend execute the following commands:
+
+```
+cd ./backend/
+npm install
+npm run start
+```
+
+#### endpoints
+
+##### actions
+
+/setbias
+/generate
+
+##### getters
+
+/islive
+/getgrid
+/getcode
+
---
+++ backend/src/app.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from 'express';
+
+import { Domain } from "./domain";
+
+
+
+// this instance will provide the internal logic for the application
+const domain: Domain = new Domain();
+
+
+// create the api and start listening
+const app = express();
+app.use(express.json());
+
+const port = 5000;
+
+// start listening 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+//////////////////////////////////
+// endpoints
+
+app.patch('/setbias', (request: Request, response: Response): any => {
+    // should we validate the bias value
+    let bias = request.body.bias;
+    let code = domain.setBias(request.body.bias);
+    return response.status(code).json({});
+});
+
+app.patch('/generate', (request: Request, response: Response): any => {
+    domain.generate();
+    return response.status(200).json({});
+});
+
+app.get('/islive', (request: Request, response: Response): any => {
+    return response.status(200).json(domain.isLive());
+});
+
+app.get('/getgrid', (request: Request, response: Response): any => {
+    return response.status(200).json(domain.getGrid());
+});
+
+app.get('/getcode', (request: Request, response: Response): any => {
+    return response.status(200).json(domain.getCode());
+});
+
+
