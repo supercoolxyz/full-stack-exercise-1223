1c04ea69c1be6d37391e8c708435599f224a6420
Mário <brifanz@hotmail.com>
2024-09-22 16:21:41 +0100

    payments page first commit. refactored the frontend structure to allow multiple pages.

---
+++ frontend/src/app/pages/payments-page/payments-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payments-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './payments-page.component.html',
+  styleUrl: './payments-page.component.scss'
+})
+export class PaymentsPageComponent {
+
+}
